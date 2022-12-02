692dc459514dead441601521b97c4eed551d7463
BrianZ26 <brian.zhao026@gmail.com>
2022-12-02 13:58:39 -0500

    A

---
+++ vite-project/array.js
@@ -0,0 +1,8 @@
+const food = [
+  {
+    name: "a",
+    cost: 1,
+    url: "https://redhousespice.com/wp-content/uploads/2021/10/Chinese-egg-fried-rice-in-a-bowl-scaled.jpg",
+  },
+];
+export const 
