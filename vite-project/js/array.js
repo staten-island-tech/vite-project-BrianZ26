ebfb2cc08bc6be901987c4a9636e3b1f54b0d4df
BrianZ26 <brian.zhao026@gmail.com>
2022-12-14 13:58:41 -0500

    A

--- vite-project/js/array.js
+++ vite-project/js/array.js
@@ -1,18 +1,30 @@
 const menu = [
   {
-    name: "Pizza",
-    cost: 1,
-    meat: false,
-    url: "https://static.toiimg.com/thumb/53110049.cms?width=1200&height=900",
-  },
-  {
-    name: "Fried Chicken",
+    name: "Egg Fried Rice",
     cost: 5,
-    meat: true,
-    url: "https://tmbidigitalassetsazure.blob.core.windows.net/rms3-prod/attachments/37/1200x1200/Crispy-Fried-Chicken_EXPS_TOHJJ22_6445_DR%20_02_03_11b.jpg",
+    meat: false,
+    url: "https://www.kitchensanctuary.com/wp-content/uploads/2021/03/Egg-fried-rice-square-FS-40.jpg",
   },
   {
-    name: "c",
+    name: "Shrimp Dumplings",
+    cost: 7,
+    meat: false,
+    url: "https://www.curiouscuisiniere.com/wp-content/uploads/2022/01/har-gow-horizontal.2.jpg",
+  },
+  {
+    name: "Beef Noodle Soup",
+    cost: 13,
+    meat: true,
+    url: "https://redhousespice.com/wp-content/uploads/2020/08/Spicy-beef-noodle-soup-foodgawker.jpg",
+  },
+  {
+    name: "Peking Duck",
+    cost: 15,
+    meat: true,
+    url: "https://redhousespice.com/wp-content/uploads/2022/01/sliced-peking-duck-with-pancakes-scaled.jpg",
+  },
+  {
+    name: "d",
     cost: 1,
     meat: true,
     url: "test",
